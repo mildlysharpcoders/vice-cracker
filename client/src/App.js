c374e993a9445d2172db364879c4b0186df0d245
Paul Black <pblack67@comcast.net>
2019-07-29 15:25:37 -0500

    Imported axios, implements hard-coded login test button

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+
+function LogInStuff(props) {
+  if (props.username) {
+    return <h1>Hi {props.username}!</h1>;
+  } else {
+    return (
+      <button type="button" onClick={props.handleButtonClick}>
+        Log Me In
+      </button>
+    );
+  }
+}
 
 class App extends Component {
+  state = {
+    username: ""
+  };
+
+  handleButtonClick = event => {
+    console.log("Login Button Clicked");
+    axios
+      .post("/api/user/login", { username: "pablo", password: "password" })
+      .then(response => {
+        console.log(response.data);
+        this.setState({ username: response.data.username });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Mildly Sharp Coders at Work!</h2>
+      <>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Mildly Sharp Coders at Work!</h2>
+          </div>
         </div>
-      </div>
+        <LogInStuff
+          username={this.state.username}
+          handleButtonClick={this.handleButtonClick}
+        />
+      </>
     );
   }
 }
