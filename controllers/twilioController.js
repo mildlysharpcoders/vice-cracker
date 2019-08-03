77a4ff7c80a6f123afac039e2b18995b5b8382c7
Paul Black <pblack67@comcast.net>
2019-08-03 13:05:58 -0500

    Refactored twilio logic into a util file

--- controllers/twilioController.js
+++ controllers/twilioController.js
@@ -1,26 +1,11 @@
-require("dotenv").config();
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_AUTH;
-const fromPhone = process.env.TWILIO_FROM_NUMBER;
-
-const client = require("twilio")(accountSid, authToken);
+const twilio = require("../utils/twilio");
 
 const twilioController = {
   createTextNotification: (request, response) => {
     console.log("twilioController.createTextNotification ", request.body);
-    sendTextMessage(request.body.message, request.body.tonumber);
+    twilio.sendTextMessage(request.body.message, request.body.tonumber);
     response.sendStatus(201);
   }
 };
-
-function sendTextMessage(message, tonumber) {
-  client.messages
-    .create({
-      body: message,
-      from: fromPhone,
-      to: tonumber
-    })
-    .then(message => console.log(message.sid));
-}
 
 module.exports = twilioController;
