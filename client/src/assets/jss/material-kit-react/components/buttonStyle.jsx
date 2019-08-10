8d23826216ff325ca0b013c9d4cc86d9869af993
elletiburon <elleharrer@gmail.com>
2019-08-09 22:32:04 -0500

    broke all the references/routes on the id page probably but it doesn't look as bad now

--- client/src/assets/jss/material-kit-react/components/buttonStyle.jsx
+++ client/src/assets/jss/material-kit-react/components/buttonStyle.jsx
@@ -23,7 +23,7 @@ import {
   successColor,
   warningColor,
   dangerColor
-} from "assets/jss/material-kit-react.jsx";
+} from "./../../material-kit-react.jsx";
 
 const buttonStyle = {
   button: {
