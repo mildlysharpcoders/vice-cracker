cbc04e0d640c9e413a48ae6cb417cccf6ca0e07f
Paul Black <pblack67@comcast.net>
2019-08-13 13:33:13 -0500

    Added exact length validations, fixed zip issue, abstracted empty validation

--- client/src/pages/CreateUser.js
+++ client/src/pages/CreateUser.js
@@ -19,16 +19,6 @@ import GridContainer from "../components/Grid/GridContainer.jsx";
 //     )
 //   }
 
-//   if (email.length < 5) {
-//     errors.push('Email should be at least 5 charcters long')
-//   }
-//   if (email.split('').filter(x => x === '@').length !== 1) {
-//     errors.push('Email should contain a @')
-//   }
-//   if (email.indexOf('.') === -1) {
-//     errors.push('Email should contain at least one dot')
-//   }
-
 class CreateUser extends Component {
   state = {
     password: "",
@@ -38,7 +28,7 @@ class CreateUser extends Component {
     address: "",
     city: "",
     state: "",
-    zipcode: "",
+    zip: "",
     phone: "",
     error: ""
   };
@@ -51,50 +41,71 @@ class CreateUser extends Component {
     });
   };
 
+  isEmpty = (item, message) => {
+    if (!item || !item.trim().length === 0) {
+      this.setState({ error: message });
+      return true;
+    }
+    return false;
+  };
+
+  isExactLength = (item, message, length) => {
+    if (item && (item.trim().length === length)) {
+      return true;
+    }
+    this.setState({ error: message });
+    return false;
+  };
+
   validate = () => {
-    // Validate stuff here!
-    if (this.state.firstname.length === 0) {
-      this.setState({ error: "First Name must not be blank" });
+    if (this.isEmpty(this.state.firstname, "First Name must not be blank")) {
       return false;
     }
 
-    if (this.state.lastname.length === 0) {
-      this.setState({ error: "Last Name must not be blank" });
+    if (this.isEmpty(this.state.lastname, "Last Name must not be blank")) {
       return false;
     }
 
-    if (this.state.email.length === 0) {
-      this.setState({ error: "Email must not be blank" });
+    if (this.isEmpty(this.state.email, "Email must not be blank")) {
       return false;
     }
 
-    if (this.state.password.length === 0) {
-      this.setState({ error: "Password must not be blank" });
+    if (this.state.email.length < 5) {
+      this.setState({ error: "Email should be at least 5 charcters long" });
       return false;
     }
 
-    if (this.state.address.length === 0) {
-      this.setState({ error: "Address must not be blank" });
+    if (this.state.email.split("").filter(x => x === "@").length !== 1) {
+      this.setState({ error: "Email should contain an @" });
       return false;
     }
 
-    if (this.state.city.length === 0) {
-      this.setState({ error: "City must not be blank" });
+    if (this.state.email.indexOf(".") === -1) {
+      this.setState({ error: "Email should contain at least one dot" });
       return false;
     }
 
-    if (this.state.state.length === 0) {
-      this.setState({ error: "State must not be blank" });
+    if (this.isEmpty(this.state.password, "Password must not be blank")) {
       return false;
     }
 
-    if (this.state.zipcode.length === 0) {
-      this.setState({ error: "Zip Code must not be blank" });
+    if (this.isEmpty(this.state.address, "Address must not be blank")) {
       return false;
     }
 
-    if (this.state.phone.length === 0) {
-      this.setState({ error: "Phone must not be blank" });
+    if (this.isEmpty(this.state.city, "City must not be blank")) {
+      return false;
+    }
+
+    if (!this.isExactLength(this.state.state, "State must be exactly 2 characters", 2)) {
+      return false;
+    }
+
+    if (!this.isExactLength(this.state.zip, "Zip Code must be exactly 5 characters", 5)) {
+      return false;
+    }
+
+    if (!this.isExactLength(this.state.phone, "Phone number must be exactly 10 digits", 10)) {
       return false;
     }
 
@@ -107,25 +118,17 @@ class CreateUser extends Component {
 
     if (this.validate()) {
       let createUserInfo = {
-        firstname: this.state.firstname,
-        lastname: this.state.lastname,
-        email: this.state.email,
-        city: this.state.city,
-        state: this.state.state,
-        address: this.state.address,
-        password: this.state.password,
-        zipcode: this.state.zipcode,
-        phone: this.state.phone
-        // firstname: this.state.firstname.trim(),
-        // lastname: this.state.lastname.trim(),
-        // email: this.state.email.trim(),
-        // city: this.state.city.trim(),
-        // state: this.state.state.trim(),
-        // address: this.state.address.trim(),
-        // password: this.state.password.trim(),
-        // zipcode: this.state.zipcode.trim(),
-        // phone: this.state.phone.trim()
+        firstname: this.state.firstname.trim(),
+        lastname: this.state.lastname.trim(),
+        email: this.state.email.trim(),
+        city: this.state.city.trim(),
+        state: this.state.state.trim(),
+        address: this.state.address.trim(),
+        password: this.state.password.trim(),
+        zip: this.state.zip.trim(),
+        phone: this.state.phone.trim()
       };
+      console.log(createUserInfo);
       API.createUser(createUserInfo)
         .then(response => {
           console.log("Created user info: " + response.data);
@@ -133,7 +136,7 @@ class CreateUser extends Component {
         })
         .catch(error => {
           console.log(error);
-          this.setState({ error: "Invalid username/password" });
+          this.setState({ error: "Email address already in use" });
         });
     }
   };
@@ -276,14 +279,14 @@ class CreateUser extends Component {
                   type="text"
                   labelText="ZIP"
                   id="float"
-                  name="zipcode"
+                  name="zip"
                   ref={zipInput => (this._zipInput = zipInput)}
                   // value={this.state.zip}
                   // onChange={this.handleInputChange}
                   inputProps={{
                     type: "text",
-                    value: this.state.zipcode,
-                    name: "zipcode",
+                    value: this.state.zip,
+                    name: "zip",
                     onChange: event => this.handleInputChange(event)
                   }}
                 />
