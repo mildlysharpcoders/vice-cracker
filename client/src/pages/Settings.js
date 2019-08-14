b9e09de921647d6881fa575690f82f268df48da6
Paul Black <pblack67@comcast.net>
2019-08-14 10:50:00 -0500

    Added input validation to the Settings page

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,121 +1,168 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-import API from '../utils/API'
-import ViceItem from '../components/ViceItem'
-import Card from '../components/Card/Card.jsx'
-import CardHeader from '../components/Card/CardHeader.jsx'
-import CardBody from '../components/Card/CardBody.jsx'
-import Dropdown from '../components/CustomDropdown/CustomDropdown.jsx'
-import CustomInput from '../components/CustomInput/CustomInput.jsx'
-import GridContainer from '../components/Grid/GridContainer.jsx'
-import GridItem from '../components/Grid/GridItem.jsx'
-import Button from '../components/CustomButtons/Button.jsx'
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import API from "../utils/API";
+import ViceItem from "../components/ViceItem";
+import Card from "../components/Card/Card.jsx";
+import CardHeader from "../components/Card/CardHeader.jsx";
+import CardBody from "../components/Card/CardBody.jsx";
+import Dropdown from "../components/CustomDropdown/CustomDropdown.jsx";
+import CustomInput from "../components/CustomInput/CustomInput.jsx";
+import GridContainer from "../components/Grid/GridContainer.jsx";
+import GridItem from "../components/Grid/GridItem.jsx";
+import Button from "../components/CustomButtons/Button.jsx";
 
 class Settings extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      name: '',
-      betteroption: 'Recipe',
-      limit: '',
-      cost: '',
-      vices: []
-    }
+      name: "",
+      betteroption: "Recipe",
+      limit: "",
+      cost: "",
+      vices: [],
+      error: ""
+    };
   }
 
   componentDidMount = () => {
-    this.loadVices()
-  }
+    this.loadVices();
+  };
 
   loadVices = () => {
-    const user = this.props.user.email
+    const user = this.props.user.email;
     if (user) {
       API.getVicesForUser(user)
         .then(response => {
           console.log(response.data.length, "vices returned");
-          this.setState({ vices: response.data })
+          this.setState({ vices: response.data });
         })
         .catch(error => {
-          console.log(error)
-        })
+          console.log(error);
+        });
     }
-  }
+  };
 
   handleInputChange = event => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     // Updating the input's state
     this.setState({
       [name]: value
-    })
-  }
+    });
+  };
 
-  handleFormSubmit = event => {
-    event.preventDefault()
-    console.log(this.props)
-    let vice = {
-      email: this.props.user.email,
-      name: this.state.name.trim(),
-      betteroption: this.state.betteroption.trim(),
-      limit: this.state.limit.trim(),
-      cost: this.state.cost.trim(),
-      weekly: [],
-      monthly: []
+  isEmpty = (item, message) => {
+    if (!item || !item.trim().length === 0) {
+      this.setState({ error: message });
+      return true;
+    }
+    return false;
+  };
+
+  validate = () => {
+    if (this.isEmpty(this.state.name, "Vice Name must not be blank")) {
+      return false;
     }
 
-    console.log(vice)
+    if (this.isEmpty(this.state.betteroption, "Healthier Option must not be blank")) {
+      return false;
+    }
 
-    API.createVice(vice)
-      .then(response => {
-        console.log('Vice Created:', response.data)
-        this.setState({
-          name: '',
-          betteroption: '',
-          limit: '',
-          cost: ''
+    if (this.isEmpty(this.state.limit, "Consumption Limit must not be blank")) {
+      return false;
+    }
+
+    if (this.state.limit.startsWith("-")) {
+      this.setState({ error: "Consumption Limit must be a positive number" });
+      return false;
+    }
+
+    if (this.state.limit.indexOf(".") !== -1) {
+      this.setState({ error: "Consumption Limit must be a whole number" });
+      return false;
+    }
+
+    if (this.isEmpty(this.state.cost, "Cost must not be blank")) {
+      return false;
+    }
+
+    if (this.state.cost.startsWith("-")) {
+      this.setState({ error: "Cost must be a positive number" });
+      return false;
+    }
+
+    this.setState({ error: "" });
+    return true;
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.validate()) {
+      console.log(this.props);
+      let vice = {
+        email: this.props.user.email,
+        name: this.state.name.trim(),
+        betteroption: this.state.betteroption.trim(),
+        limit: this.state.limit.trim(),
+        cost: this.state.cost.trim(),
+        weekly: [],
+        monthly: []
+      };
+
+      console.log(vice);
+
+      API.createVice(vice)
+        .then(response => {
+          console.log("Vice Created:", response.data);
+          this.setState({
+            name: "",
+            betteroption: "",
+            limit: "",
+            cost: ""
+          });
+          this.loadVices();
         })
-        this.loadVices()
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  };
 
   handleDeleteButtonClick = vice => {
-    console.log('Delete Vice Button Clicked for vice:', vice.name)
+    console.log("Delete Vice Button Clicked for vice:", vice.name);
     API.deleteVice(vice._id)
       .then(response => {
-        console.log(response.data)
-        this.loadVices()
+        console.log(response.data);
+        this.loadVices();
       })
       .catch(error => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
   handleIncrementButtonClick = vice => {
-    console.log('Increment Vice Button Clicked for vice:', vice)
+    console.log("Increment Vice Button Clicked for vice:", vice);
     API.createViceEvent(vice)
       .then(response => {
-        console.log(response.data)
-        this.loadVices()
+        console.log(response.data);
+        this.loadVices();
       })
       .catch(error => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
   renderRedirect = () => {
     if (!this.props.user || !this.props.user.email) {
-      return <Redirect to='/' />
+      return <Redirect to="/" />;
     }
-  }
+  };
 
   handleMenuClick = betteroption => {
-    this.setState({ betteroption })
-  }
+    this.setState({ betteroption });
+  };
 
-  render () {
+  render() {
     return (
       <Card>
         {/* {this.renderRedirect()} */}
@@ -125,18 +172,18 @@ class Settings extends Component {
             <GridItem xs={12} sm={12} md={12}>
               <CustomInput
                 // type='text'
-                labelText='Name Your Vice'
+                labelText="Name Your Vice"
                 // name='name'
                 // value={this.state.name}
                 // onChange={this.handleInputChange}
-                className='form-control'
-                placeholder='Your Vice'
-                aria-label='Username'
-                aria-describedby='addon-wrapping'
+                className="form-control"
+                placeholder="Your Vice"
+                aria-label="Username"
+                aria-describedby="addon-wrapping"
                 inputProps={{
-                  type: 'text',
+                  type: "text",
                   value: this.state.name,
-                  name: 'name',
+                  name: "name",
                   onChange: event => this.handleInputChange(event)
                 }}
               />
@@ -147,11 +194,11 @@ class Settings extends Component {
             <GridItem xs={12} sm={12} md={12}>
               <Dropdown
                 buttonText={this.state.betteroption}
-                dropdownList={['Gym', 'Recipe', "Health Food Store"]}
+                dropdownList={["Gym", "Recipe", "Health Food Store"]}
                 onClick={this.handleMenuClick}
                 buttonProps={{
                   round: true,
-                  color: 'info'
+                  color: "info"
                 }}
               />
             </GridItem>
@@ -161,18 +208,18 @@ class Settings extends Component {
             <GridItem xs={12} sm={12} md={12}>
               <CustomInput
                 // type='number'
-                labelText='Consumption'
+                labelText="Consumption"
                 // name='limit'
                 // value={this.state.limit}
                 // onChange={this.handleInputChange}
-                className='form-control'
-                placeholder='Consumption/Week'
-                aria-label='Username'
-                aria-describedby='addon-wrapping'
+                className="form-control"
+                placeholder="Consumption/Week"
+                aria-label="Username"
+                aria-describedby="addon-wrapping"
                 inputProps={{
-                  type: 'number',
+                  type: "number",
                   value: this.state.limit,
-                  name: 'limit',
+                  name: "limit",
                   onChange: event => this.handleInputChange(event)
                 }}
               />
@@ -183,29 +230,32 @@ class Settings extends Component {
             <GridItem xs={12} sm={12} md={12}>
               <CustomInput
                 // type='number'
-                labelText='Cost'
+                labelText="Cost"
                 // name='cost'
                 // value={this.state.cost}
                 // onChange={this.handleInputChange}
-                className='form-control'
-                placeholder='Cost'
-                aria-label='Username'
-                aria-describedby='addon-wrapping'
+                className="form-control"
+                placeholder="Cost"
+                aria-label="Username"
+                aria-describedby="addon-wrapping"
                 inputProps={{
-                  type: 'number',
+                  type: "number",
                   value: this.state.cost,
-                  name: 'cost',
+                  name: "cost",
                   onChange: event => this.handleInputChange(event)
                 }}
               />
             </GridItem>
           </GridContainer>
 
+          {/* TODO: Replace this dull html with something more eye-grabbing */}
+          <p>{this.state.error}</p>
+
           <Button
-            color='primary'
+            color="primary"
             round
-            type='button'
-            className='btn btn-secondary'
+            type="button"
+            className="btn btn-secondary"
             // onClick={this.handleFormSubmit}
             onClick={event => this.handleFormSubmit(event)}
           >
@@ -220,12 +270,12 @@ class Settings extends Component {
                 handleButtonClick={this.handleIncrementButtonClick}
                 handleDeleteButtonClick={this.handleDeleteButtonClick}
               />
-            )
+            );
           })}
         </CardBody>
       </Card>
-    )
+    );
   }
 }
 
-export default Settings
+export default Settings;
