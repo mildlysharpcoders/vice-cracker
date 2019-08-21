2381cc0204eca7f9c8136574b2a968ebdbb8db8c
Paul Black <pblack67@comcast.net>
2019-08-21 11:05:58 -0500

    Refactored code in an effort to fix image bug. No dice, but more organized code.

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 
-
 class noMatch extends Component {
   constructor(props) {
     super(props);
@@ -15,105 +14,144 @@ class noMatch extends Component {
       width: "50%",
       height: "30px"
     };
-    this.state = {
-      photo_x: 0,
-      photo_y: 0,
-      PhotoImg: "",
-      myCanvas: "",
-      ctx: "",
-      melon_x: 0,
-      melon_y: 0,
-      melonImg: "",
-      score: 0
-    }
-  }
 
-
-  gameStuff = () => {
-    var myCanvas = document.getElementById("myCanvas");
-    var thisctx = myCanvas.getContext("2d");
     let Photoinfo = new Image();
     let Meloninfo = new Image();
     Meloninfo.src = "/cheese-wedge.png";
     Photoinfo.src = "/something.jpg";
 
-    this.setState({
-      photo_height: Photoinfo.height,
-      melon_width: Meloninfo.width,
+    this.state = {
+      photo_x: 0,
+      photo_y: 0,
       PhotoImg: Photoinfo,
+      myCanvas: {},
+      ctx: {},
+      melon_x: 0,
+      melon_y: 0,
       melonImg: Meloninfo,
-      ctx: thisctx,
-      myCanvas: myCanvas
-    });
+      score: 0,
+      photo_height: Photoinfo.height,
+      melon_width: Meloninfo.width
+    };
+  }
 
-    var ImagesTouching = (x1, y1, img1, x2, y2, img2) => {
-      if (x1 >= x2 + img2.width || x1 + img1.width <= x2) return false;   // too far to the side
-      if (y1 >= y2 + img2.height || y1 + img1.height <= y2) return false; // too far above/below
-      return true;                                                    // otherwise, overlap   
-    }
-
-    var Do_a_Frame = () => {
-      this.state.photo_y = this.state.myCanvas.height - this.state.photo_height;
-      this.state.ctx.clearRect(0, 0, this.state.myCanvas.width, this.state.myCanvas.height);
-
-      this.state.ctx.fillStyle = "white";
-      this.state.ctx.font = "20px Arial";
-      this.state.ctx.fillText("Score: " + this.state.score, 0, 20);
-
-      this.state.ctx.drawImage(this.state.PhotoImg, this.state.photo_x, this.state.photo_y);
-
-      this.state.melon_y = this.state.melon_y + 3;
-      if (this.state.melon_y > this.state.myCanvas.height) {
-        this.state.melon_y = 0;
-        this.state.melon_x = Math.random() * (this.state.myCanvas.width - this.state.melon_width);
-      }
-      this.state.ctx.drawImage(this.state.melonImg, this.state.melon_x, this.state.melon_y);
-
-      if (ImagesTouching(this.state.photo_x, this.state.photo_y, this.state.PhotoImg, this.state.melon_x, this.state.melon_y, this.state.melonImg)) {
-        this.state.score = this.state.score + 1;
-        this.state.melon_x = -this.state.melonImg.width;
-      }
-    }
-
-
-    setInterval(Do_a_Frame, 25);
-
+  ImagesTouching = (x1, y1, img1, x2, y2, img2) => {
+    if (x1 >= x2 + img2.width || x1 + img1.width <= x2) return false; // too far to the side
+    if (y1 >= y2 + img2.height || y1 + img1.height <= y2) return false; // too far above/below
+    return true; // otherwise, overlap
   };
 
-  MyKeyDownHandler = (MyEvent) => {
-    if (MyEvent.keyCode === 37 && this.state.photo_x > 0) { this.state.photo_x = this.state.photo_x - 10; }   // left
-    if (MyEvent.keyCode === 39 && this.state.photo_x + this.state.PhotoImg.width < this.state.myCanvas.width) { this.state.photo_x = this.state.photo_x + 10; }   // right
-  }
+  Do_a_Frame = () => {
+    this.state.photo_y = this.state.myCanvas.height - this.state.photo_height;
+    this.state.ctx.clearRect(
+      0,
+      0,
+      this.state.myCanvas.width,
+      this.state.myCanvas.height
+    );
 
-  ButtonClickHandler = (MyEvent) => {
-    if (MyEvent.target.id === "leftButton" && this.state.photo_x > 0) { this.state.photo_x = this.state.photo_x - 10; }   // left
-    if (MyEvent.target.id === "rightButton" && this.state.photo_x + this.state.PhotoImg.width < this.state.myCanvas.width) { this.state.photo_x = this.state.photo_x + 10; }   // right
-  }
+    this.state.ctx.fillText("Score: " + this.state.score, 0, 20);
 
+    this.state.ctx.drawImage(
+      this.state.PhotoImg,
+      this.state.photo_x,
+      this.state.photo_y
+    );
 
+    this.state.melon_y = this.state.melon_y + 3;
+    if (this.state.melon_y > this.state.myCanvas.height) {
+      this.state.melon_y = 0;
+      this.state.melon_x =
+        Math.random() * (this.state.myCanvas.width - this.state.melon_width);
+    }
+    this.state.ctx.drawImage(
+      this.state.melonImg,
+      this.state.melon_x,
+      this.state.melon_y
+    );
+
+    if (
+      this.ImagesTouching(
+        this.state.photo_x,
+        this.state.photo_y,
+        this.state.PhotoImg,
+        this.state.melon_x,
+        this.state.melon_y,
+        this.state.melonImg
+      )
+    ) {
+      this.setState({ score: this.state.score + 1 });
+      this.setState({ melon_x: -this.state.melonImg.width });
+    }
+  };
+
+  startAnimation = () => {
+    setInterval(this.Do_a_Frame, 25);
+  };
+
+  MyKeyDownHandler = MyEvent => {
+    if (MyEvent.keyCode === 37 && this.state.photo_x > 0) {
+      this.setState({ photo_x: this.state.photo_x - 10 });
+    } // left
+    if (
+      MyEvent.keyCode === 39 &&
+      this.state.photo_x + this.state.PhotoImg.width < this.state.myCanvas.width
+    ) {
+      this.setState({ photo_x: this.state.photo_x + 10 });
+    } // right
+  };
+
+  ButtonClickHandler = MyEvent => {
+    if (MyEvent.target.id === "leftButton" && this.state.photo_x > 0) {
+      this.setState({ photo_x: this.state.photo_x - 10 });
+    } // left
+    if (
+      MyEvent.target.id === "rightButton" &&
+      this.state.photo_x + this.state.PhotoImg.width < this.state.myCanvas.width
+    ) {
+      this.setState({ photo_x: this.state.photo_x + 10 });
+    } // right
+  };
 
   componentDidMount = () => {
-    this.gameStuff();
     document.addEventListener("keydown", this.MyKeyDownHandler);
-  }
-
+    let myCanvas = document.getElementById("myCanvas");
+    let ctx = myCanvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
+    this.setState({ myCanvas, ctx });
+    setTimeout(this.startAnimation(), 5000);
+  };
 
   render() {
     return (
       <div height="100% - 40px" width="100%" className="FourPage">
-        <canvas id="myCanvas" width={window.innerWidth} height={window.innerHeight - 45} style={this.thingy}>
-
-        </canvas>
+        <canvas
+          id="myCanvas"
+          width={window.innerWidth}
+          height={window.innerHeight - 45}
+          style={this.thingy}
+        />
 
         <div style={this.thingy2}>
-          <button id="leftButton" onClick={this.ButtonClickHandler} style={this.buttonWidth}>&#8592; </button>
-          <button id="rightButton" onClick={this.ButtonClickHandler} style={this.buttonWidth}>&#8594;</button>
+          <button
+            id="leftButton"
+            onClick={this.ButtonClickHandler}
+            style={this.buttonWidth}
+          >
+            &#8592;{" "}
+          </button>
+          <button
+            id="rightButton"
+            onClick={this.ButtonClickHandler}
+            style={this.buttonWidth}
+          >
+            &#8594;
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
-
-
 
 export default noMatch;
